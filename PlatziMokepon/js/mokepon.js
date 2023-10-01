23c28d0a0cba6f4216ff40015f79f2f6e94cc736
JuanseVillegas <61911574+juanvjaramillo@users.noreply.github.com>
2023-10-01 16:25:02 -0500

    Add Mokepon project
    Se agrega el primer commit de mokepon

---
+++ PlatziMokepon/js/mokepon.js
@@ -0,0 +1,11 @@
+function seleccionarMascotaJugador(){
+    alert("Hola, bienvenidos a MOKEPON")
+}
+
+
+let btnMascotaJugador = document.getElementById("boton-mascota")
+btnMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+
+
+
